9ede124162d167489bb96a207af70ccee793ac2f
Aaahhk <schneider.n.achim@gmail.com>
2024-12-22 12:10:44 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
 
-const CACHE_NAME = 'learning-app-cache-v1';
+const CACHE_NAME = 'learning-app-cache-v1.1';
 const assetsToCache = [
-    './',
     './index.html',
     './app.js',
     './manifest.json',
+    './favicon.png',
+    './words_and_sentences.json'
 ];
 
 self.addEventListener('install', event => {
@@ -20,5 +21,17 @@ self.addEventListener('fetch', event => {
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            })
+        ))
     );
 });
